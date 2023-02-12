7f7182cd1ad6ba42f52d53517496d534c4cd9c10
bxbao87 <bxbao18@apcs.vn>
2023-02-12 16:45:22 +0700

    [part2b][phonebook] ex2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const onNameChange = (event) => {
     setNewName(event.target.value)  
@@ -15,12 +19,16 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const onFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
   const onSubmitForm = (event) => {
     event.preventDefault()
     if (persons.find(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      let tmp = persons.concat({name: newName, number: newNumber})
+      let tmp = persons.concat({name: newName, number: newNumber, id: persons.length+1})
       setPersons(tmp)
       setNewName('')
       setNewNumber('')
@@ -30,6 +38,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={newFilter} onChange={onFilterChange} /> </div>
+
+      <h2>add a new</h2>
       <form onSubmit={onSubmitForm}>
         <div>name: <input value={newName} onChange={onNameChange} /> </div>
         <div>number: <input value={newNumber} onChange={onNumberChange}/></div>
@@ -40,7 +51,7 @@ const App = () => {
 
       
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}
+      {persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase())).map(person => <div key={person.id}>{person.name} {person.number}</div>)}
     </div>
   )
 }
