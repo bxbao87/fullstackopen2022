d741109f5f2f87f7938b774afcab4d0338397106
bxbao87 <bxbao18@apcs.vn>
2023-02-19 23:03:08 +0700

    [part2d][phonebook] ex2.13

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,24 +2,19 @@ import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-import axios from 'axios'
+import personService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
   useEffect(()=>{
-    axios
-      .get("http://localhost:3001/persons")
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialData => {
+        setPersons(initialData)
       })
   }, [])
 
@@ -42,12 +37,13 @@ const App = () => {
     } else {
       let newPerson = {name: newName, number: newNumber}
 
-      axios.post("http://localhost:3001/persons", newPerson).then(response => {
-        setPersons(persons.concat(response.data))
-        // console.log("persons", persons)
-        setNewName('')
-        setNewNumber('')
-      })
+      personService
+        .create(newPerson)
+        .then(createdPerson => {
+          setPersons(persons.concat(createdPerson))
+          setNewName('')
+          setNewNumber('')
+        })
     }
   }
 
